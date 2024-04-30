24ed27f3c53ddae987f30ae5be82442dced1bc10
Lavergnat Jérémie <lavergnatj@outlook.fr>
2024-04-30 19:37:35 +0200

    add dropdown and banner props, add dropdown dynamically via table data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,10 @@ import GalleryCard from "../../components/GalleryCard/GalleryCard";
 export default function Home() {
   return (
     <main className="home">
-      <Banner />
+      <Banner
+        title="Chez vous, partout et ailleurs"
+        imageUrl="public/img/home-banner.png"
+      />
       <div className="home__housings-list">
         <GalleryCard />
       </div>
